17ff7cf3af6ac4ae9973af2e1116575d985465c3
John Dolsen <John@0587341893.wireless.umich.net>
2017-10-09 17:59:48 -0400

    dropdown creates objects on menu change

--- SurgicalToolsUnity/Assets/drop.cs
+++ SurgicalToolsUnity/Assets/drop.cs
@@ -5,13 +5,38 @@ using UnityEngine.UI;
 
 public class drop : MonoBehaviour {
 
-	// Use this for initialization
-	private void Start () {
-        this.GetComponent<Dropdown>().captionText.text = "Surgical Tools";
+    // Use this for initialization
+    private void Start()
+    {
+        if (this.GetComponent<Dropdown>() != null)
+        {
+            this.GetComponent<Dropdown>().captionText.text = "Surgical Tools";
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void DropdownHandler(int input) {
+        Debug.Log("Option selected: " + input);
+
+        if(input.Equals(0))
+        {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            Instantiate(cube);
+        }
+        else if(input.Equals(1))
+        {
+            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            Instantiate(sphere);
+        }
+        else if(input.Equals(2))
+        {
+            GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            Instantiate(plane);
+        }
+
+    }
 }
