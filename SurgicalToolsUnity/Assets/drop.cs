46f19c6bd5d41b39af8f897e8a3ce3ad95b07851
John Dolsen <John@Johns-MacBook-Pro-2.local>
2017-10-09 14:17:24 -0400

    dropdown views in unity

---
+++ SurgicalToolsUnity/Assets/drop.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class drop : MonoBehaviour {
+
+	// Use this for initialization
+	private void Start () {
+        this.GetComponent<Dropdown>().captionText.text = "Surgical Tools";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
