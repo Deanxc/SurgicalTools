d6aea1acb80eed7368a552a88cdf80f9131a19bb
dolsenj <dolsenj@umich.edu>
2017-10-11 14:48:48 -0400

    Dragging objects when created

--- SurgicalToolsUnity/Assets/DropdownMenu.cs
+++ SurgicalToolsUnity/Assets/DropdownMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class drop : MonoBehaviour {
+public class DropdownMenu : MonoBehaviour {
 
     // Use this for initialization
     private void Start()
