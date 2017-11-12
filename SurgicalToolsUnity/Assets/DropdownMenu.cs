5406bdbb752b945917924563b9ee0d08069f4cce
dolsenj <dolsenj@umich.edu>
2017-11-12 17:15:23 -0500

    Generates imported human and surgical tools

--- SurgicalToolsUnity/Assets/DropdownMenu.cs
+++ SurgicalToolsUnity/Assets/DropdownMenu.cs
@@ -6,7 +6,13 @@ using UnityEngine.UI;
 public class DropdownMenu : MonoBehaviour {
 
     public GameObject human;
-    public GameObject scalpel;
+    public GameObject forceps;
+    public GameObject graspingForceps;
+    public GameObject lancet;
+    public GameObject scissors;
+    public GameObject surgicalMirror;
+    public GameObject surgicalSaw;
+    public GameObject syringe;
 
     // Use this for initialization
     private void Start()
@@ -25,33 +31,46 @@ public class DropdownMenu : MonoBehaviour {
     public void DropdownHandler(int input) {
         Debug.Log("Option selected: " + input);
 
-        if(input.Equals(0))
-        {
-            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            cube.AddComponent<MovingObject>();
-            Instantiate(cube);
-        }
-        else if(input.Equals(1))
-        {
-            Instantiate(scalpel);
-        }
-        else if (input.Equals(2))
+        if (input.Equals(0))
         {
             Instantiate(human);
         }
-        else if(input.Equals(3))
+        else if (input.Equals(1))
         {
-            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            sphere.AddComponent<MovingObject>();
-            //Instantiate(sphere);
+            Instantiate(forceps);
         }
-        else if(input.Equals(4))
+        else if (input.Equals(2))
         {
-            GameObject capsule = GameObject.CreatePrimitive(PrimitiveType.Capsule);
-            capsule.AddComponent<MovingObject>();
-            //Instantiate(capsule);
+            Instantiate(graspingForceps);
         }
-
+        else if (input.Equals(3))
+        {
+            Instantiate(lancet);
+        }
+        else if (input.Equals(4))
+        {
+            Instantiate(scissors);
+        }
+        else if (input.Equals(5))
+        {
+            Instantiate(surgicalMirror);
+        }
+        else if (input.Equals(6))
+        {
+            Instantiate(surgicalSaw);
+        }
+        else if (input.Equals(7))
+        {
+            Instantiate(syringe);
+        }
 
     }
 }
+
+
+
+
+
+
+
+
