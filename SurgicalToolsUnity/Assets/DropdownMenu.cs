95c7357e7de985f3c419d84da699ad3529e80386
dolsenj <dolsenj@umich.edu>
2017-11-12 18:45:46 -0500

    Change sizes of objects

--- SurgicalToolsUnity/Assets/DropdownMenu.cs
+++ SurgicalToolsUnity/Assets/DropdownMenu.cs
@@ -32,49 +32,49 @@ public class DropdownMenu : MonoBehaviour {
     public void DropdownHandler(int input) {
         Debug.Log("Option selected: " + input);
 
-        if (input.Equals(0))
+        if (input.Equals(1))
         {
-            human.transform.position = new Vector3(5,0,0);
-            human.transform.localScale = new Vector3(10, 10, 10);
+            human.transform.position = new Vector3(5, -2, 0);
+            human.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
             Instantiate(human);
         }
-        else if (input.Equals(1))
+        else if (input.Equals(2))
         {
             forceps.transform.position = new Vector3(5, 0, 0);
             forceps.transform.localScale = new Vector3(10, 10, 10);
             Instantiate(forceps);
         }
-        else if (input.Equals(2))
+        else if (input.Equals(3))
         {
             graspingForceps.transform.position = new Vector3(5, 0, 0);
             graspingForceps.transform.localScale = new Vector3(10, 10, 10);
             Instantiate(graspingForceps);
         }
-        else if (input.Equals(3))
+        else if (input.Equals(4))
         {
             lancet.transform.position = new Vector3(3, 0, 0);
             lancet.transform.localScale = new Vector3(10, 10, 10);
             Instantiate(lancet);
         }
-        else if (input.Equals(4))
+        else if (input.Equals(5))
         {
             scissors.transform.position = new Vector3(-5, 0, 0);
             scissors.transform.localScale = new Vector3(10, 10, 10);
             Instantiate(scissors);
         }
-        else if (input.Equals(5))
+        else if (input.Equals(6))
         {
             surgicalMirror.transform.position = new Vector3(-3, 0, 0);
             surgicalMirror.transform.localScale = new Vector3(10, 10, 10);
             Instantiate(surgicalMirror);
         }
-        else if (input.Equals(6))
+        else if (input.Equals(7))
         {
             surgicalSaw.transform.position = new Vector3(0, 5, 0);
             surgicalSaw.transform.localScale = new Vector3(10, 10, 10);
             Instantiate(surgicalSaw);
         }
-        else if (input.Equals(7))
+        else if (input.Equals(8))
         {
             syringe.transform.position = new Vector3(0, 7, 0);
             syringe.transform.localScale = new Vector3(10, 10, 10);
