f96d59c4c31b446ff96ea4998031c075f5f1fe7a
nkathawa <nkathawa@umich.edu>
2017-12-09 12:59:24 -0500

    has the menu, menu has one option and that is forceps. it creates a forceps

--- Assets/HoloToolkitExtensions/TapToSelect.cs
+++ Assets/HoloToolkitExtensions/TapToSelect.cs
@@ -5,6 +5,18 @@ namespace LocalJoost.HoloToolkitExtensions
 {
     public class TapToSelect : MonoBehaviour, IInputClickHandler
     {
+
+
+        //public GameObject human;
+        public GameObject forceps;
+        public GameObject graspingForceps;
+        public GameObject lancet;
+        public GameObject scissors;
+        public GameObject surgicalMirror;
+        public GameObject surgicalSaw;
+        public GameObject surgicalHook;
+        public GameObject syringe;
+
         public virtual void OnInputClicked(InputEventData eventData)
         {
             if (BaseAppStateManager.IsInitialized)
@@ -13,6 +25,30 @@ namespace LocalJoost.HoloToolkitExtensions
                 if (BaseAppStateManager.Instance.SelectedGameObject != gameObject)
                 {
                     BaseAppStateManager.Instance.SelectedGameObject = gameObject;
+
+                    //////////////////////////////////////////////////////////////////////////////////////
+                    if (BaseAppStateManager.Instance.SelectedGameObject.transform.childCount > 0) 
+                    {
+                        if (BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(0).GetComponent<TextMesh>().text == "Forceps")
+                        {
+                            //GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                            //cube.transform.position = new Vector3(0, 0, 0);
+
+                            forceps.transform.position = new Vector3(2, 0, 2);
+                            forceps.transform.localScale = new Vector3(2, 2, 2);
+                            forceps.AddComponent<BoxCollider>();
+                            forceps.AddComponent<SpatialManipulator>();
+                            forceps.AddComponent<SpatialMappingCollisionDetector>();
+                            forceps.AddComponent<TapToSelect>();
+
+                            Instantiate(forceps);
+                        }
+                        else
+                        {
+                            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                            sphere.transform.position = new Vector3(0, 0, 0);
+                        }
+                    }
                 }
                 else
                 {
