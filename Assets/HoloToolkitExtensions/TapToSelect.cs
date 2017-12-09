c71c650c1dad2dc9137ce1c659a6b164d9dee3d3
nkathawa <nkathawa@umich.edu>
2017-12-09 17:42:34 -0500

    full menu, creates all tools. Tools cannot be tapped to select. Human can be tapped to select but cannot be rotated scaled or deleted usingvoice

--- Assets/HoloToolkitExtensions/TapToSelect.cs
+++ Assets/HoloToolkitExtensions/TapToSelect.cs
@@ -34,14 +34,58 @@ namespace LocalJoost.HoloToolkitExtensions
                             //GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
                             //cube.transform.position = new Vector3(0, 0, 0);
 
-                            forceps.transform.position = new Vector3(2, 0, 2);
-                            forceps.transform.localScale = new Vector3(2, 2, 2);
-                            forceps.AddComponent<BoxCollider>();
-                            forceps.AddComponent<SpatialManipulator>();
-                            forceps.AddComponent<SpatialMappingCollisionDetector>();
-                            forceps.AddComponent<TapToSelect>();
+                            //forceps.transform.position = new Vector3(2, 0, 2);
+                            //forceps.transform.localScale = new Vector3(2, 2, 2);
 
-                            Instantiate(forceps);
+                            forceps = Instantiate(forceps, new Vector3(-4, 1, 2), Quaternion.identity) as GameObject;
+
+                            //forceps.AddComponent<BoxCollider>();
+                            //forceps.AddComponent<SpatialManipulator>();
+                            //forceps.AddComponent<SpatialMappingCollisionDetector>();
+                            //forceps.AddComponent<TapToSelect>();
+                            //forceps.AddComponent<HandDraggable>();
+                            //forceps.AddComponent<GazeManager>();
+                            //forceps.AddComponent<MeshFilter>();
+                            //forceps.AddComponent<MeshRenderer>();
+
+
+
+                            
+                        }
+                        else if(BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(0).GetComponent<TextMesh>().text == "Grasping")
+                        {
+                            //graspingForceps.transform.position = new Vector3(2, 0, 2);
+                            //graspingForceps.transform.localScale = new Vector3(2, 2, 2);
+
+                            Instantiate(graspingForceps);
+
+                        }
+                        else if (BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(0).GetComponent<TextMesh>().text == "Lancet")
+                        {
+                            Instantiate(lancet);
+                        }
+                        else if (BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(0).GetComponent<TextMesh>().text == "Scissors")
+                        {
+                            Instantiate(scissors);
+                        }
+                        else if (BaseAppStateManager.Instance.SelectedGameObject.transform.childCount > 1 &&
+                            BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(1).GetComponent<TextMesh>().text == "hook")
+                        {
+                            Instantiate(surgicalHook);
+                        }
+                        else if (BaseAppStateManager.Instance.SelectedGameObject.transform.childCount > 1 &&
+                            BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(1).GetComponent<TextMesh>().text == "mirror")
+                        {
+                            Instantiate(surgicalMirror);
+                        }
+                        else if (BaseAppStateManager.Instance.SelectedGameObject.transform.childCount > 1 &&
+                            BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(1).GetComponent<TextMesh>().text == "saw")
+                        {
+                            Instantiate(surgicalSaw);
+                        }
+                        else if (BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(0).GetComponent<TextMesh>().text == "Syringe")
+                        {
+                            Instantiate(syringe);
                         }
                         else
                         {
