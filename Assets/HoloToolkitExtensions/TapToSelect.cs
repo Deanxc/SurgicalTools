2fcc48360cd48d5e403ee679379af978dc7fe607
nkathawa <nkathawa@umich.edu>
2017-12-10 16:18:37 -0500

    menu creates all the tools and they create near the menu. they also have ability to create more than once

--- Assets/HoloToolkitExtensions/TapToSelect.cs
+++ Assets/HoloToolkitExtensions/TapToSelect.cs
@@ -36,61 +36,68 @@ namespace LocalJoost.HoloToolkitExtensions
                         BaseAppStateManager.Instance.SelectedGameObject == GameObject.Find("Cube (6)") ||
                         BaseAppStateManager.Instance.SelectedGameObject == GameObject.Find("Cube (7)"))
                     {
+                        var menuPos = GameObject.Find("MenuParent").transform.position;
+                        var spawnPos = new Vector3(menuPos.x, menuPos.y + 0.5f, menuPos.z);
                         //if child count is greater than 0 AND the text is Forceps
                         if (BaseAppStateManager.Instance.SelectedGameObject.transform.childCount > 0 &&
                             BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(0).GetComponent<TextMesh>().text == "Forceps")
                         {
                             // make a forceps and add it to the collection
-                            forceps = Instantiate(forceps);
+                            //get menu pos
+                            //var menuPos = GameObject.Find("MenuParent").transform.position;
+                            //get position we want for forceps
+                            //var spawnPos = new Vector3(menuPos.x, menuPos.y + 0.5f, menuPos.z);
+
+                            forceps = Instantiate(forceps, spawnPos, Quaternion.identity);
                             forceps.transform.parent = GameObject.Find("HologramCollection").transform;
                         }
                         else if (BaseAppStateManager.Instance.SelectedGameObject.transform.childCount > 0 &&
                             BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(0).GetComponent<TextMesh>().text == "Lancet")
                         {
                             // make a forceps and add it to the collection
-                            lancet = Instantiate(lancet);
+                            lancet = Instantiate(lancet, spawnPos, Quaternion.identity);
                             lancet.transform.parent = GameObject.Find("HologramCollection").transform;
                         }
                         else if (BaseAppStateManager.Instance.SelectedGameObject.transform.childCount > 0 &&
                             BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(0).GetComponent<TextMesh>().text == "Scissors")
                         {
                             // make a forceps and add it to the collection
-                            scissors = Instantiate(scissors);
+                            scissors = Instantiate(scissors, spawnPos, Quaternion.identity);
                             scissors.transform.parent = GameObject.Find("HologramCollection").transform;
                         }
                         else if (BaseAppStateManager.Instance.SelectedGameObject.transform.childCount > 0 &&
                             BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(0).GetComponent<TextMesh>().text == "Syringe")
                         {
                             // make a forceps and add it to the collection
-                            syringe = Instantiate(syringe);
+                            syringe = Instantiate(syringe, spawnPos, Quaternion.identity);
                             syringe.transform.parent = GameObject.Find("HologramCollection").transform;
                         }
                         else if (BaseAppStateManager.Instance.SelectedGameObject.transform.childCount > 0 &&
                             BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(0).GetComponent<TextMesh>().text == "Grasping")
                         {
                             // make a forceps and add it to the collection
-                            graspingForceps = Instantiate(graspingForceps);
+                            graspingForceps = Instantiate(graspingForceps, spawnPos, Quaternion.identity);
                             graspingForceps.transform.parent = GameObject.Find("HologramCollection").transform;
                         }
                         else if (BaseAppStateManager.Instance.SelectedGameObject.transform.childCount > 1 &&
                             BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(1).GetComponent<TextMesh>().text == "hook")
                         {
                             // make a forceps and add it to the collection
-                            surgicalHook = Instantiate(surgicalHook);
+                            surgicalHook = Instantiate(surgicalHook, spawnPos, Quaternion.identity);
                             surgicalHook.transform.parent = GameObject.Find("HologramCollection").transform;
                         }
                         else if (BaseAppStateManager.Instance.SelectedGameObject.transform.childCount > 1 &&
                             BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(1).GetComponent<TextMesh>().text == "mirror")
                         {
                             // make a forceps and add it to the collection
-                            surgicalMirror = Instantiate(surgicalMirror);
+                            surgicalMirror = Instantiate(surgicalMirror, spawnPos, Quaternion.identity);
                             surgicalMirror.transform.parent = GameObject.Find("HologramCollection").transform;
                         }
                         else if (BaseAppStateManager.Instance.SelectedGameObject.transform.childCount > 1 &&
                             BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(1).GetComponent<TextMesh>().text == "saw")
                         {
                             // make a forceps and add it to the collection
-                            surgicalSaw = Instantiate(surgicalSaw);
+                            surgicalSaw = Instantiate(surgicalSaw, spawnPos, Quaternion.identity);
                             surgicalSaw.transform.parent = GameObject.Find("HologramCollection").transform;
                         }
                         else { }
