4817fa126489995f8742b4a2f2227aa6a5e74354
nkathawa <nkathawa@umich.edu>
2017-12-09 23:49:17 -0500

    menu with one square that creates a tool w/full func., and there is a human model with full func.

--- Assets/HoloToolkitExtensions/TapToSelect.cs
+++ Assets/HoloToolkitExtensions/TapToSelect.cs
@@ -5,6 +5,8 @@ namespace LocalJoost.HoloToolkitExtensions
 {
     public class TapToSelect : MonoBehaviour, IInputClickHandler
     {
+        public GameObject forceps;
+
         public virtual void OnInputClicked(InputEventData eventData)
         {
             if (BaseAppStateManager.IsInitialized)
@@ -13,6 +15,18 @@ namespace LocalJoost.HoloToolkitExtensions
                 if (BaseAppStateManager.Instance.SelectedGameObject != gameObject)
                 {
                     BaseAppStateManager.Instance.SelectedGameObject = gameObject;
+                    //if it is not a human
+                    if (BaseAppStateManager.Instance.SelectedGameObject != GameObject.Find("HuMan"))
+                    {
+                        //if child count is greater than 0 AND the text is Forceps
+                        if (BaseAppStateManager.Instance.SelectedGameObject.transform.childCount > 0 &&
+                            BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(0).GetComponent<TextMesh>().text == "Forceps")
+                        {
+                            // make a forceps and add it to the collection
+                            forceps = Instantiate(forceps);
+                            forceps.transform.parent = GameObject.Find("HologramCollection").transform;
+                        }
+                    }
                 }
                 else
                 {
