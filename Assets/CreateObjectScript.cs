779386089be343be56328bb24974481de20cecad
nkathawa <nkathawa@umich.edu>
2017-12-09 22:16:44 -0500

    capusle created at runtime and it works with all func. It is also under the HologramCollection

---
+++ Assets/CreateObjectScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using LocalJoost.HoloToolkitExtensions;
+
+public class CreateObjectScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var cap = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        cap.AddComponent<BoxCollider>();
+        cap.AddComponent<MeshRenderer>();
+        cap.AddComponent<MeshFilter>();
+        cap.AddComponent<HandDraggable>();
+        cap.AddComponent<GazeManager>();
+        cap.AddComponent<TapToSelect>();
+        cap.AddComponent<SpatialManipulator>();
+        cap.AddComponent<SpatialMappingCollisionDetector>();
+        cap.transform.position = new Vector3(2, 0, 2);
+        cap.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        cap.transform.parent = GameObject.Find("HologramCollection").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/HoloToolkitExtensions/TapToSelect.cs
+++ Assets/HoloToolkitExtensions/TapToSelect.cs
@@ -5,18 +5,6 @@ namespace LocalJoost.HoloToolkitExtensions
 {
     public class TapToSelect : MonoBehaviour, IInputClickHandler
     {
-
-
-        //public GameObject human;
-        public GameObject forceps;
-        public GameObject graspingForceps;
-        public GameObject lancet;
-        public GameObject scissors;
-        public GameObject surgicalMirror;
-        public GameObject surgicalSaw;
-        public GameObject surgicalHook;
-        public GameObject syringe;
-
         public virtual void OnInputClicked(InputEventData eventData)
         {
             if (BaseAppStateManager.IsInitialized)
@@ -25,74 +13,6 @@ namespace LocalJoost.HoloToolkitExtensions
                 if (BaseAppStateManager.Instance.SelectedGameObject != gameObject)
                 {
                     BaseAppStateManager.Instance.SelectedGameObject = gameObject;
-
-                    //////////////////////////////////////////////////////////////////////////////////////
-                    if (BaseAppStateManager.Instance.SelectedGameObject.transform.childCount > 0) 
-                    {
-                        if (BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(0).GetComponent<TextMesh>().text == "Forceps")
-                        {
-                            //GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                            //cube.transform.position = new Vector3(0, 0, 0);
-
-                            //forceps.transform.position = new Vector3(2, 0, 2);
-                            //forceps.transform.localScale = new Vector3(2, 2, 2);
-
-                            forceps = Instantiate(forceps, new Vector3(-4, 1, 2), Quaternion.identity) as GameObject;
-
-                            //forceps.AddComponent<BoxCollider>();
-                            //forceps.AddComponent<SpatialManipulator>();
-                            //forceps.AddComponent<SpatialMappingCollisionDetector>();
-                            //forceps.AddComponent<TapToSelect>();
-                            //forceps.AddComponent<HandDraggable>();
-                            //forceps.AddComponent<GazeManager>();
-                            //forceps.AddComponent<MeshFilter>();
-                            //forceps.AddComponent<MeshRenderer>();
-
-
-
-                            
-                        }
-                        else if(BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(0).GetComponent<TextMesh>().text == "Grasping")
-                        {
-                            //graspingForceps.transform.position = new Vector3(2, 0, 2);
-                            //graspingForceps.transform.localScale = new Vector3(2, 2, 2);
-
-                            Instantiate(graspingForceps);
-
-                        }
-                        else if (BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(0).GetComponent<TextMesh>().text == "Lancet")
-                        {
-                            Instantiate(lancet);
-                        }
-                        else if (BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(0).GetComponent<TextMesh>().text == "Scissors")
-                        {
-                            Instantiate(scissors);
-                        }
-                        else if (BaseAppStateManager.Instance.SelectedGameObject.transform.childCount > 1 &&
-                            BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(1).GetComponent<TextMesh>().text == "hook")
-                        {
-                            Instantiate(surgicalHook);
-                        }
-                        else if (BaseAppStateManager.Instance.SelectedGameObject.transform.childCount > 1 &&
-                            BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(1).GetComponent<TextMesh>().text == "mirror")
-                        {
-                            Instantiate(surgicalMirror);
-                        }
-                        else if (BaseAppStateManager.Instance.SelectedGameObject.transform.childCount > 1 &&
-                            BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(1).GetComponent<TextMesh>().text == "saw")
-                        {
-                            Instantiate(surgicalSaw);
-                        }
-                        else if (BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(0).GetComponent<TextMesh>().text == "Syringe")
-                        {
-                            Instantiate(syringe);
-                        }
-                        else
-                        {
-                            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                            sphere.transform.position = new Vector3(0, 0, 0);
-                        }
-                    }
                 }
                 else
                 {
