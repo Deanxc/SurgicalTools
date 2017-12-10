1217eaa5445f33ec0de6eaf482c2bcb14d8d8362
nkathawa <nkathawa@umich.edu>
2017-12-10 01:35:15 -0500

    we now have the tool menu. creates all the tools. all tools can move/rotate/resize/delete, and so can the human. some small bugs mentioned in slack, not sure how to fix atm

--- Assets/HoloToolkitExtensions/TapToSelect.cs
+++ Assets/HoloToolkitExtensions/TapToSelect.cs
@@ -6,6 +6,13 @@ namespace LocalJoost.HoloToolkitExtensions
     public class TapToSelect : MonoBehaviour, IInputClickHandler
     {
         public GameObject forceps;
+        public GameObject graspingForceps;
+        public GameObject lancet;
+        public GameObject scissors;
+        public GameObject surgicalMirror;
+        public GameObject surgicalSaw;
+        public GameObject surgicalHook;
+        public GameObject syringe;
 
         public virtual void OnInputClicked(InputEventData eventData)
         {
@@ -26,6 +33,56 @@ namespace LocalJoost.HoloToolkitExtensions
                             forceps = Instantiate(forceps);
                             forceps.transform.parent = GameObject.Find("HologramCollection").transform;
                         }
+                        else if (BaseAppStateManager.Instance.SelectedGameObject.transform.childCount > 0 &&
+                            BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(0).GetComponent<TextMesh>().text == "Lancet")
+                        {
+                            // make a forceps and add it to the collection
+                            lancet = Instantiate(lancet);
+                            lancet.transform.parent = GameObject.Find("HologramCollection").transform;
+                        }
+                        else if (BaseAppStateManager.Instance.SelectedGameObject.transform.childCount > 0 &&
+                            BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(0).GetComponent<TextMesh>().text == "Scissors")
+                        {
+                            // make a forceps and add it to the collection
+                            scissors = Instantiate(scissors);
+                            scissors.transform.parent = GameObject.Find("HologramCollection").transform;
+                        }
+                        else if (BaseAppStateManager.Instance.SelectedGameObject.transform.childCount > 0 &&
+                            BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(0).GetComponent<TextMesh>().text == "Syringe")
+                        {
+                            // make a forceps and add it to the collection
+                            syringe = Instantiate(syringe);
+                            syringe.transform.parent = GameObject.Find("HologramCollection").transform;
+                        }
+                        else if (BaseAppStateManager.Instance.SelectedGameObject.transform.childCount > 0 &&
+                            BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(0).GetComponent<TextMesh>().text == "Grasping")
+                        {
+                            // make a forceps and add it to the collection
+                            graspingForceps = Instantiate(graspingForceps);
+                            graspingForceps.transform.parent = GameObject.Find("HologramCollection").transform;
+                        }
+                        else if (BaseAppStateManager.Instance.SelectedGameObject.transform.childCount > 1 &&
+                            BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(1).GetComponent<TextMesh>().text == "hook")
+                        {
+                            // make a forceps and add it to the collection
+                            surgicalHook = Instantiate(surgicalHook);
+                            surgicalHook.transform.parent = GameObject.Find("HologramCollection").transform;
+                        }
+                        else if (BaseAppStateManager.Instance.SelectedGameObject.transform.childCount > 1 &&
+                            BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(1).GetComponent<TextMesh>().text == "mirror")
+                        {
+                            // make a forceps and add it to the collection
+                            surgicalMirror = Instantiate(surgicalMirror);
+                            surgicalMirror.transform.parent = GameObject.Find("HologramCollection").transform;
+                        }
+                        else if (BaseAppStateManager.Instance.SelectedGameObject.transform.childCount > 1 &&
+                            BaseAppStateManager.Instance.SelectedGameObject.transform.GetChild(1).GetComponent<TextMesh>().text == "saw")
+                        {
+                            // make a forceps and add it to the collection
+                            surgicalSaw = Instantiate(surgicalSaw);
+                            surgicalSaw.transform.parent = GameObject.Find("HologramCollection").transform;
+                        }
+                        else { }
                     }
                 }
                 else
